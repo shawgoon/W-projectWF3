89fe100cbb6791630f414aa4fcd0c640ab8a4429
OwlCup9 <tatiana.garagnan94@hotmail.com>
2017-01-30 09:44:53 +0100

    modif deleteModel

--- app/Model/DeleteModel.php
+++ app/Model/DeleteModel.php
@@ -3,44 +3,23 @@
 namespace Model;
 
 use \W\Model\Model;
+use \W\Model\ConnexionModel;
 
 class DeleteModel extends Model
 {
     public function deleteMsg($validItinerary)
     {
-
         $sql = "DELETE FROM avis WHERE id = " . $_POST['msgId']; //ajouter un input hidden
-
-        $deleteSuccess = $instance->exec($sql);
-
-        if ($deleteSuccess) {
-            $message = "Message effacé";
-        } else {
-            $message = "Erreur";
-        }
     }
 
     public function deleteInformation()
     {
         $sql = "DELETE FROM renseignement WHERE id = ". $_POST['infoId'];
-
-        $deleteSuccess = $instance->exec($sql);
-
-        if ($deleteSuccess) {
-            $message = "Effacement confirmé !";
-        } else {
-            $message = "Erreur";        }
     }
 
-    public function deleteItinerary (){
+    public function deleteItinerary ()
+    {
         $sql = "DELETE FROM trajet WHERE id = ". $_POST['ItinId'];
-
-        $deleteSuccess = $instance->exec($sql);
-
-        if ($deleteSuccess) {
-            $message = "Effacement confirmé !";
-        } else {
-            $message = "Erreur";        }
     }
 }
 
